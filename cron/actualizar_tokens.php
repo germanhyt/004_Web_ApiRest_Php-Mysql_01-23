fce89e84777c437ff869167c80032f51ff6e4ce2
germanhyt <85075441+germanhyt@users.noreply.github.com>
2023-01-14 17:44:39 -0500

    Add files in project

--- cron/actualizar_tokens.php
+++ cron/actualizar_tokens.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__.'/../class/taken.class.php';
+    require_once __DIR__.'/../class/token.class.php';
     $_token = new token;
     date_default_timezone_set('America/Lima');
     $fecha = date('Y-m-d H:i');
